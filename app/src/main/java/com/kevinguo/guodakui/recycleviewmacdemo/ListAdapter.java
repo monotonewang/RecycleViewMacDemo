db654a3996836475effa222198bbf48d9d804478
montotone <zgjxfuqingwang@gmail.com>
2017-03-03 11:43:43 +0800

    Initial commit

---
+++ app/src/main/java/com/kevinguo/guodakui/recycleviewmacdemo/ListAdapter.java
@@ -0,0 +1,124 @@
+package com.kevinguo.guodakui.recycleviewmacdemo;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by guodakui on 16/11/5.
+ */
+
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ItemViewHolder> {
+
+    private List<String> mDatas;
+    private LayoutInflater minflater;
+    private OnItemClickLister onItemClickLister;
+
+    public ListAdapter(Context context, List<String> data) {
+        this.mDatas = data;
+        minflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        ItemViewHolder holder = new ItemViewHolder(minflater.inflate(
+                R.layout.item_layout, parent, false));
+        return holder;
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    public void onBindViewHolder(final ItemViewHolder holder, int position) {
+        holder.mtextView.setText(mDatas.get(position));
+        if (onItemClickLister != null) {
+            if (!holder.mtextView.hasOnClickListeners()) {
+                holder.mtextView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        int pos = holder.getLayoutPosition();
+                        onItemClickLister.OnItemClick(view, pos);
+                    }
+                });
+
+                holder.mtextView.setOnLongClickListener(new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View view) {
+                        int pos = holder.getLayoutPosition();
+                        onItemClickLister.OnItemLongClick(view, pos);
+                        return true;
+                    }
+                });
+                holder.mImageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        int pos = holder.getLayoutPosition();
+                        onItemClickLister.onImageClick(view, pos);
+                    }
+                });
+            }
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDatas.size();
+    }
+
+    public void setOnitermClickLister(OnItemClickLister onItemClickLister) {
+        this.onItemClickLister = onItemClickLister;
+    }
+
+    public void add(int position, String value) {
+        if (position > mDatas.size()) {
+            position = mDatas.size();
+        }
+        if (position < 0) {
+            position = 0;
+        }
+        mDatas.add(position, value);
+
+        notifyItemInserted(position);
+    }
+
+    public String remove(int position) {
+        if (position > mDatas.size()) {
+            return null;
+        }
+        if (position < 0) {
+            return null;
+        }
+        String value = mDatas.remove(position);
+        notifyItemRemoved(position);
+
+        return value;
+    }
+
+    interface OnItemClickLister {
+         void OnItemClick(View view, int position);
+
+         void OnItemLongClick(View view, int position);
+
+        void onImageClick(View view, int position);
+    }
+
+    class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView mtextView;
+        private ImageView mImageView;
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+            mtextView = (TextView) itemView.findViewById(R.id.textview);
+            mImageView = (ImageView) itemView.findViewById(R.id.imageView);
+        }
+    }
+}
